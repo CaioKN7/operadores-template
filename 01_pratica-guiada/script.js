0884ad598a5b825c446d90b05d7c092629215078
CaioKN7 <galdinocaiost@gmail.com>
2023-07-12 21:11:21 -0300

    Prática 1 concluída

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,12 @@
 //CODIGO AQUI
+
+const valorDoProduto = +prompt("Insira o valor do produto?") //Number
+const porcentagemDoDesconto = Number (prompt("Digite a porcentagem do desconto?"))//Number
+
+const valorDoDesconto = valorDoProduto*(porcentagemDoDesconto/100)
+console.log("Valor do desconto é de R$",valorDoDesconto)
+
+
+
+const valorDoProdutoComDesconto = valorDoProduto - valorDoDesconto
+console.log("O Valor do produto com desconto é de R$",valorDoProdutoComDesconto)
